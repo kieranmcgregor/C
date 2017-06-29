820d261a995f43b522b51bc04ebdfab13bd6d956
Kieran McGregor <kieranmcgregor@gmail.com>
2017-06-29 14:24:03 +0900

    Temperature conversion using function completed

--- ANSI_C/Chapter1/Ex1-15_tc_func/tc_func.c
+++ ANSI_C/Chapter1/Ex1-15_tc_func/tc_func.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 
 /* convert fahrenheit to celsius using a function */
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
+float temp_converter(fahrenheit)
+{
+    float celsius = (5.0/9.0) * (fahrenheit - 32);
+    return celsius;
+}
 
 int main()
 {
+    float fahr, celsius;
+    printf("Fahrenheit to Celsius Conversion Table\nF\tC\n");
+
+    for (fahr = LOWER; fahr <= UPPER; fahr += STEP)
+    {
+        celsius = temp_converter(fahr);
+        printf("%3.1f\t%6.1f\n", fahr, celsius);
+    }
+
     return 0;
 }

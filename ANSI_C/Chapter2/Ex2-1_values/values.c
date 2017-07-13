a517b660ff103164698904bd3d601dd07d7a9a7d
Kieran McGregor <kieranmcgregor@gmail.com>
2017-07-13 11:07:01 +0900

    Exercise 2-1 complete

--- ANSI_C/Chapter2/Ex2-1_values/values.c
+++ ANSI_C/Chapter2/Ex2-1_values/values.c
@@ -5,8 +5,8 @@ long long power(int base, int exp);
 
 int main()
 {
+    unsigned char uc;
     char c;
-    signed char sc;
     short s;
     unsigned short us;
     int i;
@@ -15,73 +15,76 @@ int main()
     unsigned long long ul;
 
     long long temp_l;
-    int idx, temp_i;
+    int idx, temp_i, temp_c;
 
-    s = sc = 1;
-    i = l = us = ui = ul = 1;
-    temp_i = temp_l = 0;
+    uc = c = 1;
+    s = i = l = us = ui = ul = 1;
+    temp_i = temp_c = temp_l = 0;
 
-    printf("Let's go!\nchar:\n");
-    calculate_char(c);
-    printf("signed char:\n");
-    calculate_char(sc);
+    printf("Let's go!\n\nchar:\n");
+    for (idx = 1; c > 0; idx++)
+    {
+        temp_c = c = power(2, idx) - 1;
+        c++;
+    }
+    printf("%d bits with range\n%d to %d\n\n", idx, c, temp_c);
 
     printf("short int:\n");
     for (idx = 1; s > 0; idx++)
     {
-        temp_i = s = ((power(2, idx) / 2) - 1);
+        temp_i = s = power(2, idx) - 1;
         s++;
     }
-    printf("%d %d %d\n", s, (idx - 1), temp_i);
+    printf("%d bits with range\n%d to %d\n\n", idx, s, temp_i);
 
     printf("int:\n");
     for (idx = 1; i > 0; idx++)
     {
-        temp_i = i = ((power(2, idx) / 2) - 1);
+        temp_i = i = power(2, idx) - 1;
         i++;
     }
-    printf("%d %d %d\n", i, (idx - 1), temp_i);
+    printf("%d bits with range\n%d to %d\n\n", idx, i, temp_i);
+
 
     printf("long int:\n");
     for (idx = 1; l > 0; idx++)
     {
-        temp_l = l = ((power(2, idx) / 2) - 1);
+        temp_l = l = power(2, idx) - 1;
         l++;
     }
-    printf("%lld %d %lld\n", l, (idx - 1), temp_l);
+    printf("%d bits with range\n%lld to %lld\n\n", idx, l, temp_l);
+
+    printf("unsigned char:\n");
+    for (idx = 1; uc > 0; idx++)
+    {
+        temp_c = uc = power(2, idx) - 1;
+        uc++;
+    }
+    printf("%d bits with range\n%d to %d\n\n", (idx - 1), uc, temp_c);
 
     printf("unsigned short int:\n");
     for (idx = 1; us > 0; idx++)
     {
-        temp_i = us = (power(2, idx) - 1);
+        temp_i = us = power(2, idx) - 1;
         us++;
     }
-    printf("%u %d %u\n", us, (idx - 1), temp_i);
+    printf("%d bits with range\n%u to %u\n\n", (idx - 1), us, temp_i);
 
     printf("unsigned int:\n");
     for (idx = 1; ui > 0; idx++)
     {
-        temp_i = ui = (power(2, idx) - 1);
+        temp_i = ui = power(2, idx) - 1;
         ui++;
     }
-    printf("%u %d %u\n", ui, (idx - 1), temp_i);
+    printf("%d bits with range\n%u to %u\n\n", (idx - 1), ui, temp_i);
 
     printf("unsigned long int:\n");
     for (idx = 1; ul > 0; idx++)
     {
-        temp_l = ul = (power(2, idx) - 1);
+        temp_l = ul = power(2, idx) - 1;
         ul++;
     }
-    printf("%llu %d %llu\n", ul, (idx - 1), temp_l);
-}
-
-void calculate_char(char c)
-{
-    while (c > 0)
-    {
-        c++;
-    }
-    printf("%c %c\n", c, (c - 1));
+    printf("%d bits with range\n%llu to %llu\n", (idx - 1), ul, temp_l);
 }
 
 long long power(int base, int exponent)

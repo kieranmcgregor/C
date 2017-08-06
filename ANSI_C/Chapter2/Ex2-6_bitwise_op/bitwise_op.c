2605c4083b73ec240c666b87b8f690244c91b0d5
Kieran McGregor <kieranmcgregor@gmail.com>
2017-08-07 00:23:29 +0900

    Working Exercise 2-6

--- ANSI_C/Chapter2/Ex2-6_bitwise_op/bitwise_op.c
+++ ANSI_C/Chapter2/Ex2-6_bitwise_op/bitwise_op.c
@@ -1,25 +1,32 @@
 #include <stdio.h>
+#include <math.h>
 
 #define MAXLINE 1000
+#define MAXIN 4
 
 int get_line(char line [], int lim);
-unsigned getbits(unsigned x, int p, int n);
+void get_values(int numbers[], char line[], int len);
+unsigned getbits(unsigned x, int p, int n, unsigned y);
 
 int main()
 {
     int i;
-    unsigned int y;
+    unsigned int answer;
     char line[MAXLINE]; // current line
     int len; // length of current line
+    int numbers[MAXIN]; // numbers in current line
 
-    printf("Please enter an integer value 'x' at position 'p' with shift 'n':\n*Press 'CTRL + d' to exit*\n");
+    printf("Please enter an integer value 'x' for setting, the start position 'p' (0 - 3, right to left),\n");
+    printf("the number of bits 'n' and an integer value 'y' for getting all separated by spaces:\n*Press 'CTRL + d' to exit*\n");
 
     while ((len = get_line(line, MAXLINE)) > 0)
     {
         printf("%d, %s", len, line);
 
-        y = getbits(line[0], line[1], line[2]);
-        printf("%d\n", y);
+        get_values(numbers, line, len);
+
+        answer = getbits(numbers[0], numbers[1], numbers[2], numbers[3]);
+        printf("%d\n", answer);
     }
     return 0;
 }
@@ -44,7 +51,92 @@ int get_line(char line[], int lim)
     return i;
 }
 
-unsigned getbits(unsigned x, int p,int n)
+void get_values(int numbers[], char line[], int len)
 {
-    return (x >> (p + 1 - n)) & ~(~0 << n);
+    int i, j, nums_i, num_i, c;
+    int num;
+    int base = 10;
+    int value, exponent;
+    int number[MAXLINE];
+
+    nums_i = 0;
+    num_i = 0;
+    value = 0;
+
+    for (i = 0; i < len; i++)
+    {
+
+        if (line[i] >= '0' && line[i] <= '9')
+        {
+            num = line[i] - '0';
+            number[num_i++] = num;
+        }
+        else
+        {
+            for (j = 0; j < num_i; j++)
+            {
+                exponent = (num_i - (j + 1));
+                value += (number[j]) * pow(base, exponent);
+                number[j] = '\0';
+            }
+
+            if (nums_i < MAXIN)
+            {
+                numbers[nums_i++] = value;
+                value = 0;
+            }
+            else
+            {
+                printf("Too many inputs, accepting first four values ");
+
+                for (j = 0; j < MAXIN; j++)
+                {
+                    if (j == (MAXIN - 1))
+                    {
+                        printf("%d.\n", numbers[j]);
+                    }
+                    else
+                    {
+                        printf("%d, ", numbers[j]);
+                    }
+                }
+                break;
+            }
+        }
+    }
+}
+
+unsigned getbits(unsigned x, int p, int n, unsigned y)
+{
+    if (p < 0)
+    {
+        p = 0;
+        printf("Value for 'p' less than 0, p equals %d\n", p);
+    }
+    else if (p > 3)
+    {
+        p = p % 4;
+        printf("Value for 'p' greater than 3, p equals %d\n", p);
+    }
+
+    if (n < 1)
+    {
+        n = 1;
+        printf("Value for 'n' less than 1, n equals %d\n", n);
+    }
+    else if (n > 4)
+    {
+        n = ((n - 1) % 4) + 1;
+        printf("Value for 'n' greater than 4, n equals %d\n", n);
+    }
+
+    printf("\nx = %d, p = %d, n = %d, y = %d\n", x, p, n, y);
+
+    l_shift = (4 - n);
+    r_shift = (3 - p);
+
+    y << l_shift;
+    y >> r_shift;
+
+    return (x >> ((p + 1) - n)) & ~(~0 << n);
 }
